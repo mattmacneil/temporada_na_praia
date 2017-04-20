90fcf2449a6d1f191a962af1da8f35df9beb613d
Matt MacNeil <matt@mattmacneil.com>
2017-04-20 16:42:26 -0400

    Initial commit with existing files.

---
+++ index.php
@@ -0,0 +1,482 @@
+<?php
+    include "head.php";
+    include "nav.php";
+?>
+
+<div style="max-width: 2000px; margin: 0 auto;">
+
+<div id="masthead">
+
+	<div id="masthead-background">
+
+		<div id="masthead-map">
+
+			<div id="masthead-map-overlay">
+
+
+				<div id="masthead-map-left">
+
+					<div class="masthead-logo">
+
+						<a href="index.php">
+							<img src="images/hero-logo.png" alt="Temporada na Praia Homepage" />
+						</a>
+
+					</div><!-- mastheadlogo -->
+
+
+					<div class="masthead-miniheadline">
+
+						<span>Escolha uma praia para exibir pousadas aluguer:<br />
+						&rarr; &rarr; &rarr;</span>
+
+					</div><!-- miniheadline -->
+
+					<div class="mapmarker marker-01">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-01">
+						<a href="praia-do-felix.php">Praia do F&eacute;lix</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-02">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-02">
+						<a href="praia-itambuca.php">Praia Itamambuca</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-03">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-03">
+						<a href="praia-do-pereque-acu.php">Praia do Perequ&ecirc; A&ccedil;&ugrave;</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-04">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-04">
+						<a href="praia-do-itagua.php">Praia do Itagua</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-05">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-05">
+						<a href="praia-vermelha-do-centro.php">P. Vermelha do Centro</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-06">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-06">
+						<a href="praia-grande.php">Praia Grande</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-07">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-07">
+						<a href="praia-das-toninhas.php">Praia das Toninhas</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-08">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-08">
+						<a href="praia-do-pereque-mirim.php">Praia do Pereque Mirim</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-09">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-09">
+						<a href="praia-do-maranduba.php">Praia do Maranduba</a>
+					</div><!-- maplabel -->
+
+					<div class="mapmarker marker-10">
+					<i class="fa fa-star"></i>
+					</div><!-- mapmarker -->
+					<div class="maplabel label-10">
+						<a href="praia-da-lagoinha.php">Praia da Lagoinha</a>
+					</div><!-- maplabel -->
+
+					<div id="masthead-mobileselect">
+
+						<span>Escolha uma praia para exibir pousadas aluguer:</span>
+
+						<div class="selekt">
+						  <select class="selekt">
+						    <option value="XXX">Praia do Felix</option>
+						    <option value="XXX">Praia do Itagua</option>
+						    <option value="XXX">Praia Itambuca</option>
+						    <option value="XXX">Praia Grande</option>
+						    <option value="XXX">Praia do Pereque Acu</option>
+						    <option value="XXX">Praia Vermelha do Centro</option>
+						    <option value="XXX">Praia das Toninhas</option>
+						    <option value="XXX">Praia do Pereque Mirim</option>
+						    <option value="XXX">Praia do Maranduba</option>
+						    <option value="XXX">Praia da Lagoinha</option>
+						  </select>
+						</div>
+
+						<div class="masthead-mobileselect-button">
+
+							aluguel de vista
+
+						</div><!-- masthead mobileselect button -->
+
+					</div><!-- masthead mobile select -->
+
+				</div><!-- masthead map left -->
+
+				<div id="masthead-map-right">
+
+					<div class="hero-language-flag">
+
+						<div class="hero-language-flag-inner">
+
+							<div class="hero-language-cell">
+								<img src="images/hero-language-cell-portuguese.png" alt="Set Language to Portuguese" />PT
+							</div><!-- hero language cell -->
+
+							<div class="hero-language-cell middle-cell">
+								<img src="images/hero-language-cell-english.png" alt="Set Language to English" />EN
+							</div><!-- hero language cell -->
+
+							<div class="hero-language-cell">
+								<img src="images/hero-language-cell-spanish.png" alt="Set Language to Spanish" />SP
+							</div><!-- hero language cell -->
+
+						</div><!-- hero language flag inner -->
+
+						<div class="hero-language-tail-left"></div><!-- hero tail -->
+						<div class="hero-language-tail-right"></div><!-- hero tail -->
+
+					</div><!-- hero language flag -->
+
+					<div class="masthead-headline">
+
+						<div class="masthead-headline-align">
+
+							<h1>&larr; Explore Praias e Pousadas.</h1>
+
+						</div><!-- masthead headline align -->
+
+					</div><!-- masthead headline -->
+
+				</div><!-- masthead map right -->
+
+				<div class="clearfix"></div>
+
+			</div><!-- masthead map overlay -->
+
+		</div><!-- masthead map -->
+
+		<div id="undermast">
+
+			<div class="undermast-cell undermast-cell-1">
+
+				<div class="undermast-cell-overlay">
+
+					<div class="undermast-novo-snipe">
+						Novo
+					</div><!-- undermast novo snipe -->
+
+					Pousada Cascais<br />
+					<span>Aluguer em Praia do Itagua</span>
+
+				</div><!-- undermast cell overlay -->
+				
+			</div><!-- undermast cell -->
+
+			<div class="undermast-cell undermast-center undermast-cell-2">
+
+				<div class="undermast-cell-overlay">
+
+					<div class="undermast-novo-snipe">
+						Novo
+					</div><!-- undermast novo snipe -->
+
+					Solar do Lagoina<br />
+					<span>Aluguer em Praia da Lagoina</span>
+
+				</div><!-- undermast cell overlay -->
+				
+			</div><!-- undermast cell -->
+
+
+			<div class="undermast-cell undermast-cell-3">
+
+				<div class="undermast-cell-overlay">
+
+					<div class="undermast-novo-snipe">
+						Novo
+					</div><!-- undermast novo snipe -->
+
+					Hotel Pagurus<br />
+					<span>Aluguer em Praia Grande</span>
+
+				</div><!-- undermast cell overlay -->
+				
+			</div><!-- undermast cell -->
+
+			<div class="clearfix"></div>
+
+			<div class="undermast-cell undermast-cell-4" style="margin-bottom: 0;">
+
+				<div class="undermast-cell-overlay">
+
+					<span>Praia em Destaque:</span><br />
+					Praia do Tenório
+
+				</div><!-- undermast cell overlay -->
+
+			</div><!-- undermast cell -->
+
+			<div class="undermast-cell undermast-center undermast-cell-5" style="margin-bottom: 0;">
+
+				<div class="undermast-cell-overlay">
+
+					<span>Praia em Destaque:</span><br />
+					Praia das Toninhas
+
+				</div><!-- undermast cell overlay -->
+
+			</div><!-- undermast cell -->
+
+
+			<div class="undermast-cell undermast-cell-6" style="margin-bottom: 0;">
+
+				<div class="undermast-cell-overlay">
+
+					<span>Voltar ao Mapa;</span><br />
+					Acessar Todos
+
+				</div><!-- undermast cell overlay -->
+
+			</div><!-- undermast cell -->
+
+			<div class="clearfix"></div>
+
+		</div><!-- undermast -->
+
+	</div><!-- masthead background -->
+
+</div><!-- masthead -->
+
+<section id="landing-options">
+
+	<span>Eu estou procurando uma...</span><br />
+
+	<div class="landing-options-fields">
+
+		<div class="select-style">
+		  <select class="select-style-1">
+		    <option value="both">Pousada ou Hotel</option>
+		    <option value="pousadas">Solitário Pousadas</option>
+		    <option value="hotels">Solitário Hotels</option>
+		  </select>
+		</div>
+
+		<div class="select-style-2 select-middle">
+		  <select class="select-style-2">
+		    <option value="both">Quaisquer Actividades</option>
+		    <option value="pousadas">Mergulho</option>
+		    <option value="hotels">Pescaria</option>
+		    <option value="both">Vida Noturna</option>
+		    <option value="pousadas">Quiosques</option>
+		    <option value="hotels">Scuba</option>
+		    <option value="hotels">para Crianças</option>
+		  </select>
+		</div>
+
+		<div class="select-style-3">
+		  <select>
+		    <option value="both">Por uma noite</option>
+		    <option value="pousadas">Para duas noites</option>
+		    <option value="hotels">Por três noites</option>
+		    <option value="both">Por quatro noites</option>
+		    <option value="pousadas">Por cinco noites</option>
+		    <option value="hotels">Para seis noites</option>
+		    <option value="hotels">Durante sete ou mais noites</option>
+		  </select>
+		</div>
+
+		<div class="clearfix"></div>
+
+	</div><!-- landing options fields -->
+
+	<div class="landing-options-button">
+
+		Encontrar o seu próximo pousada!
+
+	</div><!-- landing option button -->
+
+	<div class="clearfix"></div>
+
+</section><!-- landing options section -->
+
+<section id="landing-bigfeatures">
+
+	<div class="landing-bigfeatures-left landing-bigfeatures-left-1">
+
+		<div class="landing-bigfeatures-date">
+			<div class="landing-bigfeatures-date-align"><?php echo date("d.m"); ?></div><!-- landing bigfeatures date align -->
+		</div><!-- landing bigfeatures date -->
+
+	</div><!-- landing bigfeatures left -->
+
+	<div class="landing-bigfeatures-right">
+
+		<span>O oceano é tudo o que eu quero ser. Bonito, misterioso, selvagem e livre...</span>
+
+		Ou sr es nome cuja capa pe. Arrojado ja gritando amoravel consulta as. Gratidao disposto portador os de lh acceites ja. Ha no humilhante aconselhar eu carvalhido ou capitulada. Com sob feito teria santa poria seres teu vae gesto. Cabo ella pau luz ter modo digo pae ande. Apagar eu ii se propoe pensou. Visitar mandaes sem ahi sua comprar. Lar nome quem vir que sao taes mola dado. 
+
+		<div class="landing-bigfeatures-readmore">
+
+			leia esta registro &rarr;
+
+		</div><!-- landing bigfeatures readmore -->
+
+	</div><!-- landing bigfeatures right -->
+
+	<div class="clearfix"></div>
+
+</section><!-- landing bigfeatures section -->
+
+<section id="landing-bigfeatures">
+
+	<div class="landing-bigfeatures-left landing-bigfeatures-left-2">
+
+		<div class="landing-bigfeatures-date">
+			<div class="landing-bigfeatures-date-align"><?php echo date("d.m", time() - 60 * 60 * 24); ?></div><!-- landing bigfeatures date align -->
+		</div><!-- landing bigfeatures date -->
+
+	</div><!-- landing bigfeatures left -->
+
+	<div class="landing-bigfeatures-right">
+
+		<span>Para sair com o pôr do sol em uma praia vazia é abraçar verdadeiramente a sua solidão.</span>
+
+		Vou differente ala mim pittoresca retumbante hypothecas. Comecam enormes da as esmolas creados. Do ve annos exija murro preto sabem morre. Sou tyrannia limitara por singular com. Vol has acho peor ora paz faze. Resume dignos ar va sr vadios. Pechincha era submetter reclamado faz lhe dar. Considerar nao satisfeito vim trabalhado por silenciosa vendadisse. 
+
+		<div class="landing-bigfeatures-readmore">
+
+			leia esta registro &rarr;
+
+		</div><!-- landing bigfeatures readmore -->
+
+	</div><!-- landing bigfeatures right -->
+
+	<div class="clearfix"></div>
+
+</section><!-- landing bigfeatures section -->
+
+<section id="landing-bigfeatures" style="padding-bottom: 0;">
+
+	<div class="landing-bigfeatures-left landing-bigfeatures-left-3">
+
+		<div class="landing-bigfeatures-date">
+			<div class="landing-bigfeatures-date-align"><?php echo date("d.m", time() - 60 * 60 * 96); ?></div><!-- landing bigfeatures date align -->
+		</div><!-- landing bigfeatures date -->
+
+	</div><!-- landing bigfeatures left -->
+
+	<div class="landing-bigfeatures-right">
+
+		<span>Não se pode recolher todas as belas conchas na praia.</span>
+
+		Queriam corrido ou la poderes adorava. Suppor tu em sr pe dictar centro plebeu saibas. Riquissimo ostentacao na apreciavel ou sanctuario trabalhado ca ao atormentar. Tereis melhor ar la porque vendam recebe. Podia teras olhal ha tarde os rugir oh. Modesto rir has uso assente imagina acoitar bem. Odio se cujo hein os pelo ja dias novo te. Victoria sua fallemos offerece nao. 
+
+		<div class="landing-bigfeatures-readmore">
+
+			leia esta registro &rarr;
+
+		</div><!-- landing bigfeatures readmore -->
+
+	</div><!-- landing bigfeatures right -->
+
+	<div class="clearfix"></div>
+
+</section><!-- landing bigfeatures section -->
+
+
+<section id="topfooter">
+
+	<div class="topfooter-cell">
+
+		<div class="topfooter-cell-snipe">
+			<i class="fa fa-home"></i>
+		</div><!-- topfooter cell snipe -->
+
+		<p class="topfooter-bignumber">144<span>+</span></p>
+
+		<div class="topfooter-cell-text">
+			Quartos disponíveis em todas as praias
+		</div><!-- topfooter cell text -->
+
+	</div><!-- topfooter cell -->
+
+	<div class="topfooter-cell topfooter-cell-middle">
+
+		<div class="topfooter-cell-snipe">
+			<i class="fa fa-money"></i>
+		</div><!-- topfooter cell snipe -->
+
+		<p class="topfooter-bignumber"><span>R</span>$88</p>
+
+		<div class="topfooter-cell-text">
+			Preço médio por noite em Ubatuba!
+		</div><!-- topfooter cell text -->
+
+	</div><!-- topfooter cell -->
+
+	<div class="topfooter-cell">
+
+		<div class="topfooter-cell-snipe">
+			<i class="fa fa-sun-o"></i>
+		</div><!-- topfooter cell snipe -->
+
+		<p class="topfooter-bignumber">+28&deg;</p>
+
+		<div class="topfooter-cell-text">
+			Temperatura atual em Praia Grande!
+		</div><!-- topfooter cell text -->
+
+	</div><!-- topfooter cell -->
+
+</section><!-- top footer section -->
+
+<section id="middlefooter">
+
+	<div class="middlefooter-speechbubble">
+
+		<div class="middlefooter-speechbubble-overlay">
+
+			Ubatuba &eacute; t&atilde;o extensa que parece imposs&iacute;vel conhecer todas as praia, e quanto mais voc&ecirc; conhece mais curioso fica! N&atilde;o deixe de conhecer a praia da Sununga e suas lendas e a famosa e maravilhosa Ilha Anchieta!
+
+			<div class="middlefooter-speechbubble-tail"></div>
+
+		</div><!-- middlefooter speechbubble overlay -->
+
+	</div><!-- middlefooter speechbubble -->
+
+	<div class="middlefooter-speechbubble-name">
+
+		&mdash; Alessandro Campos, Fundador do "Temporada na Praia"<br />
+
+	</div><!-- middlefooter speechbubble name -->
+
+</section><!-- middle footer section -->
+
+
+</div>
+<?php 
+
+    include "footer.php";
+
+?>
+
+<div id="grave"></div>
